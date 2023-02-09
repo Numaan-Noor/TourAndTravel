391afe9afe16b2851b77c95f04860a1a2bd17f4c
Numaan-Noor <numaannoor81@gmail.com>
2023-02-09 20:22:45 +0530

    updated

--- TourAndTravel/settings.py
+++ TourAndTravel/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'TourAndTravel.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -119,8 +119,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, '/static/')
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '/Static/')
+STATIC_URL = '/Static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, '/media/')
 MEDIA_URL = '/media/'
