002af8360abf4b8afd2889b74dae84b72d88b2fa
Numaan-Noor <numaannoor81@gmail.com>
2023-02-12 01:02:43 +0530

    new update

--- TourAndTravel/settings.py
+++ TourAndTravel/settings.py
@@ -77,9 +77,12 @@ WSGI_APPLICATION = 'TourAndTravel.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR / 'db.sqlite3'),
-
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'ttdb',
+        'USER': 'postgres',
+        'PASSWORD': 'database',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
