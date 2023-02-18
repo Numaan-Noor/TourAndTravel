c34ab1ebfebb229189af463136550b4b96b459ed
Numaan-Noor <numaannoor81@gmail.com>
2023-02-18 20:17:41 +0530

    changes 0.3

--- home/views.py
+++ home/views.py
@@ -1,17 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import *
-from django.contrib.auth.models import User
-from django.contrib.auth import authenticate, login
-from django.http import JsonResponse
-import json
-import datetime
-from django.contrib import messages
-# from .utils import cartData, guestOrder
-from django.contrib.auth import logout as django_logout
-from django.http import HttpResponse
-from django.core.mail import send_mail, BadHeaderError
-from django import forms
-from django.conf import settings
+
 
 # wall = wallpaper.objects.filter(categories_id=pk)
 
